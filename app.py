c0020d95d3df92b0cdd7ec508075a0a67b9c43b7
Gideonmonday27 <gideonmonday298@gmail.com>
2026-07-31 12:23:18 +0100

    first commit

--- app.py
+++ app.py
@@ -6,3 +6,9 @@ import streamlit as st
 import tensorflow as tf
 from PIL import Image
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+
+MODEL_PATH = "model/skin_classifier.keras"
+LABELS_PATH = "model/labels.json"
+BLANK_IMAGE_STD_CUTOFF = 5.0
+
+st.set_page_config(page_title="Tinea vs Candidiasis Classifier", layout="centered")
