cc5938964c9c0ff574906234915abd96b2f8fb02
hakhihiero <harrisonakhihiero21@gmail.com>
2026-07-31 12:06:46 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,8 @@
+import json
+import os
+
+import numpy as np
+import streamlit as st
+import tensorflow as tf
+from PIL import Image
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
